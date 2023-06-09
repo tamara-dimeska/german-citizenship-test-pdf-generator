aaf24a90db6db369e2b185ad75ee0202ba5b3b36
Tamara D <tamara.dimeska.94@gmail.com>
2023-06-09 09:45:37 +0200

    Project setup, create script and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # german-citizenship-test-pdf-generator
+
+The purpose of this project is to help me and people like me, that study better when they have the material in paper.
+
+## What does the script do?
+1. The script opens the official website where the library of possible questions for the German naturalization test is
+2. Goes through all the quesstions in the library
+3. Takes a screenshot of all unanswered questions
+4. Select one answer, takes a screenshot of the answers of the questions
+5. Merges all unanswered questions in one pdf `alle-fragen.pdf`
+6. Merges all answers in one pdf `alle-antworten.pdf`
+
+The final PDFs can be found in the `output/` directory.
+
+## How to run the script?
+1. Checkout the repo
+2. Run `npm install`
+3. Run `npm run create-pdf`
+
+Before running open `create-pdf.ts` file and make sure that:
+1. The element locators haven't changed
+2. The number of questions is still the same
+3. The base URl is still the same
+
+Good luck and happy studying! 
---
+++ create-pdf.ts
@@ -0,0 +1,87 @@
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+import path from 'path';
+import PDFMerger from 'pdf-merger-js';
+
+const directory = 'output';
+const baseUrl = 'https://oet.bamf.de/ords/oetut/f?p=514:1:0';
+
+// Elements
+// Since I don't have access to the source code, before running the script make sure that the text values of the buttons are still the same
+const startButtonLocator = '[value="Zum Fragenkatalog"]';
+const nextButtonLocator = '[value="nächste Aufgabe >"]';
+const answerLocator = '.t3data';
+
+// Make sure that the number of questions hasn't changed
+const noQuestions = 310;
+
+// Remove any existing files in the output directory
+fs.readdir(directory, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fs.unlink(path.join(directory, file), (err) => {
+      if (err) throw err;
+    });
+  }
+});
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--lang=en-EN'],
+  });
+  const page = await browser.newPage();
+
+  await page.goto(baseUrl, {
+    waitUntil: 'load',
+  });
+  await page.pdf({ path: `${directory}/home-page.pdf` });
+
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    page.click(startButtonLocator),
+  ]);
+
+  // Go through all the questions and generate a pdf file for each question and answer
+  for (let i = 1; i <= noQuestions; i++) {
+    await page.pdf({ path: `${directory}/frage-${i}.pdf` });
+
+    // We are always selecting the last answer, just so that the answers are displayed
+    const answer = await page.$$(answerLocator);
+    await answer[10].click(); // Each question has 4 answers, which is a table with 12 cells. The 11th element is the selection button for the last question
+    await page.pdf({ path: `${directory}/antwort-${i}.pdf` });
+
+    // The last question doesn't have next button
+    if (i != noQuestions) {
+      await Promise.all([
+        page.waitForNavigation({ waitUntil: 'networkidle0' }),
+        page.click(nextButtonLocator),
+      ]);
+    }
+  }
+
+  await browser.close();
+
+  // Merge and save all questions and answers in two separate pdf files
+  const mergerQuestions = new PDFMerger();
+  const mergerAnswers = new PDFMerger();
+
+  fs.readdirSync(directory).forEach(async (file) => {
+    if (file.includes('frage')) {
+      await mergerQuestions.add(`${directory}/${file}`);
+    } else if (file.includes('antwort')) {
+      await mergerAnswers.add(`${directory}/${file}`);
+    }
+  });
+
+  await mergerQuestions.save(`${directory}/alle-fragen.pdf`);
+  await mergerAnswers.save(`${directory}/alle-antworten.pdf`);
+
+  // Export the merged PDF as a nodejs Buffer
+  let mergedPdfBuffer = await mergerQuestions.saveAsBuffer();
+  fs.writeFileSync(`${directory}/alle-fragen.pdf`, mergedPdfBuffer);
+
+  mergedPdfBuffer = await mergerAnswers.saveAsBuffer();
+  fs.writeFileSync(`${directory}/alle-antworten.pdf`, mergedPdfBuffer);
+})();
